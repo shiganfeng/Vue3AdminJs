a6a4aea3eadca85c7fb8b2b99998d8d4fe5b5d7a
shiganfeng <2454729834@qq.com>
2021-12-29 22:02:23 +0800

    commit

---
+++ src/views/account/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login">
+        登录
+        <a-button type="primary">Primary Button</a-button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+<style lang="scss" scoped>
+</style>
